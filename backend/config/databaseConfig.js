4398f815ff971c170a015244b3e6a78be2b209d1
donoftime2018 <donoftime2018@gmail.com>
2026-08-16 22:40:27 -0400

    mongodb atlas uri used for DB

--- backend/config/databaseConfig.js
+++ backend/config/databaseConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 function connectDB()
 {
-    mongoose.connect(process.env.MONGODB_URI).then(()=>{console.log("Connected")})
+    mongoose.connect(process.env.MONGODB_ATLAS_URI).then(()=>{console.log("Connected")})
 }
 
 module.exports = connectDB
