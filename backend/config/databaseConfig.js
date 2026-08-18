8794a6ac3439879fc19872d2ea12fff424e22342
donoftime2018 <donoftime2018@gmail.com>
2026-08-18 00:18:34 -0400

    using Atlas mongodb url

--- backend/config/databaseConfig.js
+++ backend/config/databaseConfig.js
@@ -4,7 +4,7 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 function connectDB()
 {
-    mongoose.connect(process.env.MONGODB_URI).then(()=>{console.log("Connected")})
+    mongoose.connect(process.env.MONGODB_ATLAS_URI).then(()=>{console.log("Connected")})
 }
 
 module.exports = connectDB
