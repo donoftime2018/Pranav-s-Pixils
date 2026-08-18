a386ee89043ceeb0678c8081689937388f907543
donoftime2018 <donoftime2018@gmail.com>
2026-08-17 20:10:20 -0400

    dns servers for mongodb atlas

--- backend/config/databaseConfig.js
+++ backend/config/databaseConfig.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
+const dns = require('dns')
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 function connectDB()
 {
-    mongoose.connect(process.env.MONGODB_ATLAS_URI).then(()=>{console.log("Connected")})
+    mongoose.connect(process.env.MONGODB_URI).then(()=>{console.log("Connected")})
 }
 
 module.exports = connectDB
