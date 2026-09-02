593a06fecac73b16bfba8f388c189e24d6eabd3b
donoftime2018 <donoftime2018@gmail.com>
2026-09-01 22:09:32 -0400

    made route and controller for updating user profile information

--- backend/controllers/userAuthControllers.js
+++ backend/controllers/userAuthControllers.js
@@ -1,4 +1,3 @@
-const uploadFile = require('../middleware/fileUpload')
 const User = require('../models/User')
 
 exports.register = async(req, res, next)=> {
@@ -34,4 +33,58 @@ exports.register = async(req, res, next)=> {
 
     res.status(200).json({newUser: user})
     
+}
+
+exports.updateProfileInfo = async(req, res, next)=> {
+    const {id} = req.params
+    console.log(id)
+    const userDoc = await User.findById(id).exec()
+    console.log(userDoc)
+    const {username, email, fName, lName, password} = req.body
+
+    console.log(username)
+    console.log(email)
+    console.log(fName)
+    console.log(lName)
+    console.log(password)
+    const imageFile = req.file
+
+    if (username)
+    {
+        userDoc.username = username
+    }
+    if (email)
+    {
+        userDoc.email = email
+
+    }
+    if (fName)
+    {
+        userDoc.firstName = fName
+    }
+    if (lName)
+    {
+        userDoc.lastName = lName
+    }
+    if (password)
+    {
+        userDoc.password = password
+    }
+    if (imageFile)
+    {
+        console.log(imageFile.path)
+        userDoc.profilePictureURL = imageFile.path
+    }
+
+    if (userDoc.isModified())
+        await userDoc.save()
+
+    console.log(userDoc)
+
+    res.status(200).json(userDoc)
+}
+
+async function updateProfilePic(req, res, next)
+{
+
 }
--- backend/routes/userAuthRoutes.js
+++ backend/routes/userAuthRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const upload = require('../middleware/multerSetup')
-const {register} = require('../controllers/userAuthControllers')
+const {register, updateProfileInfo} = require('../controllers/userAuthControllers')
 
 router.post("/register", upload.single("file"), register)
+router.patch("/updateProfile/:id", upload.single("file"), updateProfileInfo)
 
 module.exports = router
